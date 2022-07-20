6a1b17f19ed90b4f8db9f09604c1782e72150cbc
Douglas <douglaswnogueira@hotmai.com>
2022-07-20 15:56:30 -0300

    Análise da renda

--- calculadoraFinanciamento.py
+++ calculadoraFinanciamento.py
@@ -6,3 +6,12 @@ i = 0.021
 
 pmt = pv * ((1 + i) ** parcelas * i) / ((1 + i) ** parcelas - 1)
 
+if pmt > (renda * 0.3):
+    print("O valor da parcela ultrapassa a margem permitida! ")
+else:
+    print("O empréstimo pode ser realizado!")
+    continuar = str(input("Deseja enviar a proposta? [S/N] ")).strip()[0]
+    if continuar == "s":
+        print("Proposta enviada para análise!")
+    else:
+        print("Processo encerrado.")
